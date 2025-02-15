715912b110beb3797034aa8310b9f6656d469b04
kyaxtel <kyaxtel@siue.edu>
2025-02-14 22:08:09 -0600

    Book Done

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -11,6 +11,11 @@ public class Apple : MonoBehaviour
     {
         if ( transform.position.y < bottomY ) {
             Destroy( this.gameObject );
+
+            // get a reference to the ApplePicker component of Main Camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            // Call the public AppleMissed() method of apScript
+            apScript.AppleMissed();
         }    
     }
 }
---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class ScoreCounter : MonoBehaviour
+{
+    [Header("Dynamic")]
+    public int score = 0;
+    private TextMeshProUGUI uiText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        uiText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uiText.text = score.ToString( "#,0" ); // This 0 is a zero!
+    }
+}
